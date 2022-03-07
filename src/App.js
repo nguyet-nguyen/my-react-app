70c4f27bed465cf2eb107e86cc31dee91fff491b
nguyet-nguyen <nguyet.nguyen@nfq.asia>
2022-03-07 09:55:00 +0700

    update header component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+
 import './App.css';
 import Header from './components/Header';
-import Product from './components/Product';
 import Footer from './components/Footer';
-import TestState from './components/testHook/TestState'
-import HookFile from './components/testHook/HookFile'
 
 function App() {
   return ( 
     <div>
-        <Router>
-            <Switch>
-                <Route path='/' exact component={Header}>
-                </Route>
-                <Route path='/product' exact component={Product}></Route>
-            </Switch>
-        </Router>
-        <HookFile/>
-        <TestState/>
+        <Header/>
         <Footer/>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,41 @@
 import React from 'react'
 import './../App.css'
 import logo from './../img/icon&logo/logo.PNG'
-import { Link } from 'react-router-dom'
+import TestState from './testHook/TestState'
+import HookFile from './testHook/HookFile'
+import { Link,  BrowserRouter as Router,
+  Route,} from 'react-router-dom'
 export default function Header() {
   return (
-    <header id="showcase">
-        <div className="header">
-          <div className="column logo-header animate__animated animate__flipInX">
-            <img src={logo} className="ui medium image" />
+    <Router>
+      <header id="showcase">
+          <div className="header">
+            <div className="column logo-header animate__animated animate__flipInX">
+              <img src={logo} className="ui medium image" />
+            </div>
+            <nav className="ui stackable inverted menu header-menu doubling">
+            
+              <Link to='/' className="item active">
+                  Home
+                </Link>
+                <Link to='/hookfile' className="item">
+                Messages
+                </Link>
+                <a className="item">
+                  Friends
+                </a>
+                <a className="item">
+                  Messages
+                </a>
+                <a className="item">
+                  Messages
+                </a>
+              
+            </nav>
           </div>
-          <nav className="ui stackable inverted menu header-menu doubling">
-            <Link to='/' className="item active">
-              Home
-            </Link>
-            <Link to='/product' className="item">
-            Messages
-            </Link>
-            <a className="item">
-              Friends
-            </a>
-            <a className="item">
-              Messages
-            </a>
-            <a className="item">
-              Messages
-            </a>
-          </nav>
-        </div>
       </header>
+      <Route path='/' exact component={TestState}></Route>
+      <Route path='/hookfile' exact component={HookFile}></Route>
+    </Router>
   )
 }
